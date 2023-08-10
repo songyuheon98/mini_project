b6283cc3eb1c90b425b312606c7a7b8dd1241e00
Song YuHeon <songyuheon2750@gmail.com>
2023-08-10 10:43:06 +0900

    no message

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://songyuheon2750:2028sus300djr@cluster0.mcsffwd.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
-
-
 import certifi
 
 ca=certifi.where()
 
-client = MongoClient("mongodb+srv://test:test@cluster0.15fhovx.mongodb.net/test", tlsCAFile=ca)
-db = client.dbsparta_plus_week4
+client = MongoClient('mongodb+srv://songyuheon2750:2028sus300djr@cluster0.mcsffwd.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
 
 # JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
 # 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
@@ -32,9 +30,37 @@ import hashlib
 
 @app.route('/')
 def home():
-    
     return render_template('main_login_fail.html')
     
+
+
+@app.route('/join', methods=['GET'])
+def register():
+    return render_template('join.html')
+
+# [회원가입 API]
+# id, pw, nickname을 받아서, mongoDB에 저장합니다.
+# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
+@app.route('/api/join', methods=['POST'])
+def api_register():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    nickname_receive = request.form['nickname_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    print(pw_hash)
+
+    db.mini_project.insert_one({
+        'id': id_receive,
+        'pw': pw_hash, 
+        'nick': nickname_receive
+        })
+
+    return jsonify({'result': 'success'})
+
+@app.route('/')
+def home():
+    return render_template('main.html')
 
 
 @app.route("/login", methods=["POST"])
@@ -72,7 +98,5 @@ def login_success():
 
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
-
-
 #==================================================================================
+    app.run('0.0.0.0', port=5000, debug=True)
